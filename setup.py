67d266f36c4a79f28fb5d4425067609a6bfb7945
vitowu <chenhaowu@link.cuhk.edu.cn>
2024-01-31 19:03:31 +0800

    upload project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = "PointPillars"
+
+setup(
+    name="PointPillars",
+    version=VERSION,
+    author="vitowu",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+)
